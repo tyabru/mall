bb0f72d7c18ea470f3625c6edda4333b980734a6
pijiangdada <56331575+pijiangdada@users.noreply.github.com>
2023-05-01 22:08:35 +0800

    添加商品功能实现
    --story=1001240 --user=lzp 添加商品 https://www.tapd.cn/49918443/s/1001528

--- mall_web_admin/src/views/pms/product/components/ProductSaleDetail.vue
+++ mall_web_admin/src/views/pms/product/components/ProductSaleDetail.vue
@@ -4,19 +4,19 @@
       <el-form-item label="赠送积分：">
         <el-input v-model="value.giftPoint"></el-input>
       </el-form-item>
-      <el-form-item label="赠送成长值：">
+      <!-- <el-form-item label="赠送成长值：">
         <el-input v-model="value.giftGrowth"></el-input>
-      </el-form-item>
-      <el-form-item label="积分购买限制：">
+      </el-form-item> -->
+      <!-- <el-form-item label="积分购买限制：">
         <el-input v-model="value.usePointLimit"></el-input>
-      </el-form-item>
-      <el-form-item label="预告商品：">
+      </el-form-item> -->
+      <!-- <el-form-item label="预告商品：">
         <el-switch
           v-model="value.previewStatus"
           :active-value="1"
           :inactive-value="0">
         </el-switch>
-      </el-form-item>
+      </el-form-item> -->
       <el-form-item label="商品上架：">
         <el-switch
           v-model="value.publishStatus"
@@ -57,7 +57,7 @@
       <el-form-item label="商品备注：">
         <el-input v-model="value.note" type="textarea" :autoSize="true"></el-input>
       </el-form-item>
-      <el-form-item label="选择优惠方式：">
+      <!-- <el-form-item label="选择优惠方式：">
         <el-radio-group v-model="value.promotionType" size="small">
           <el-radio-button :label="0">无优惠</el-radio-button>
           <el-radio-button :label="1">特惠促销</el-radio-button>
@@ -156,7 +156,7 @@
             </template>
           </el-table-column>
         </el-table>
-      </el-form-item>
+      </el-form-item> -->
       <el-form-item style="text-align: center">
         <el-button size="medium" @click="handlePrev">上一步，填写商品信息</el-button>
         <el-button type="primary" size="medium" @click="handleNext">下一步，填写商品属性</el-button>
