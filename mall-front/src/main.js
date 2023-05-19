0244f0ac4832ebc0aff7099b55588304baa5c94f
pijiangdada <56331575+pijiangdada@users.noreply.github.com>
2023-05-19 15:52:20 +0800

    购物车删除功能实现
    --story=1001466 --user=lzp 购物车删除 https://www.tapd.cn/49918443/s/1002180

--- mall-front/src/main.js
+++ mall-front/src/main.js
@@ -1,103 +1,30 @@
 import Vue from 'vue'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueLazyLoad from 'vue-lazyload'
-import VueCookie from 'vue-cookie'
-import { Message,Autocomplete } from 'element-ui'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// npm install --save font-awesome
-import 'font-awesome/css/font-awesome.min.css'
-import store from './store'  // 全局变量
-import App from './App.vue' 
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import VCharts from 'v-charts'
 
-// import env from './env'
-// mock开关
-const mock = false;
-if(mock){
-  require('./mock/api');
-}
+import '@/styles/index.scss' // global css
 
-axios.defaults.baseURL = '/api';
-axios.defaults.timeout = 8000; 
+import App from './App'
+import router from './router'
+import store from './store'
 
-// 根据环境变量获取不同的请求地址
-// axios.defaults.baseURL = env.baseURL;
+import '@/icons' // icon
+import '@/permission' // permission control
 
-function getCookie(c_name){
-  if (document.cookie.length>0){
-      let c_start=document.cookie.indexOf(c_name + "=")
-      if (c_start!=-1){ 
-          c_start=c_start + c_name.length+1 
-          let c_end=document.cookie.indexOf(";",c_start)
-          if (c_end==-1) c_end=document.cookie.length
-              return unescape(document.cookie.substring(c_start,c_end))
-          } 
-      }
-  return ""
-}
-// request
-// axios的拦截器   jwt+spring security
-axios.interceptors.request.use(config => {
-  // jwt令牌
-  var token= getCookie("logintoken");
-  window.console.log(token);
-  if (token !=undefined) {
-    config.headers['Authorization'] = token; // 让每个请求携带自定义token 请根据实际情况自行修改
-  } 
-  return config
-}, error => {
-  // Do something with request error
-  Promise.reject(error)
-})
+Vue.use(ElementUI, { locale })
+Vue.use(VCharts)
 
-
-
-
-
-// 接口错误拦截
-axios.interceptors.response.use(function(response){ 
-  let res = response.data;
-  let path = location.hash;   
-
-  if(res.code==undefined){
-    return res;
-  }
-
-  // 处理CommonResult逻辑
-  if(res.code == 200){
-    return res.data;
-  }else if(res.code==401 || res.code==403|| res.code==600){  //|| res.code==401 || res.code==403 
-    if (path != '#/index'){
-     
-      window.location.href = '/#/login';
-    }
-    return Promise.reject(res);
-  }else{
-    Message.warning(res.message);
-    return Promise.reject(res);
-  }
-},(error)=>{ 
-  Message.error(error.message);
-  return Promise.reject(error);
-});
-
-Vue.use(Autocomplete);
-Vue.use(VueAxios,axios);
-Vue.use(VueCookie);
-Vue.use(VueLazyLoad,{
-  loading:'/imgs/loading-svg/loading-bars.svg',
-  error:'/imgs/nopic.png'
-})
-Vue.prototype.$message = Message;
 Vue.config.productionTip = false
 
-
-// 创建VUe根实例
 new Vue({
-  store,
+  el: '#app',
   router,
-  render: h => h(App),
-}).$mount('#app')
-
-
+  store,
+  template: '<App/>',
+  components: { App }
+})
